4d5e094c1872a7d17a47db3bb516644ea73359fe
Madinamamedova93 <madinamamedova93@gmail.com>
2022-10-19 19:35:00 +0300

    Create stereosolver.h

---
+++ testOpenCV/stereosolver.h
@@ -0,0 +1,35 @@
+#ifndef STEREOGRAMSOLVER_H
+#define STEREOGRAMSOLVER_H
+#include <mutex>
+#include <thread>
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+#define MIN_REPEAT_SEGMENTS_COUNT 4
+#define MAX_REPEAT_SEGMENTS_COUNT 20
+#define MATCH_BLOCKS_WIDTH 10
+
+
+std::vector<cv::Point3f> convertMatToPointsCloud(const cv::Mat& img);
+
+class stereogramSolver {
+ public:
+  stereogramSolver();
+  ~stereogramSolver();
+
+  cv::Mat reconstructDepthMap(const cv::Mat& stereogram, int countThreads = 1);
+
+ private:
+  int findRepeatOffset(const cv::Mat& stereogram) const;
+  void reconstructDepthMapPartImg(const cv::Mat partOfStereogram,
+                                  cv::Mat* outDepthMap,
+                                  int offset,
+                                  int threadIndex);
+
+  int m_countThreads;
+  std::vector<std::thread*> m_threads;
+
+  mutable std::mutex m_PrintLocker;
+};
+#endif
