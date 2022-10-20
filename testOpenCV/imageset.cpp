5511ff4cfdbafdf19aa9cad8a08307fd780c5454
Madinamamedova93 <madinamamedova93@gmail.com>
2022-10-20 22:59:28 +0300

    Update imageset.cpp

--- testOpenCV/imageset.cpp
+++ testOpenCV/imageset.cpp
@@ -14,35 +14,13 @@ ImageSet::ImageSet(const std::string &path)
     Imgpath = path;
     fs::directory_entry directory{ path };
 
-    for (const auto& entry : fs::directory_iterator{ directory })
-    {
+    for (const auto& entry : fs::directory_iterator{ directory }) {
         std::string filename = entry.path().string();
-        if (is_right_type(filename))
-        {
-            filenames.insert(entry.path().string());
-        }
+        filenames.insert(entry.path().string());
     }
 
     currentFile = *(filenames.begin());
 
-}
-
-bool ImageSet::is_right_type(const std::string& filename)
-{
-    FILE* file = fopen(filename.c_str(), "r");
-
-    if (file == NULL)
-        return false;
-
-    unsigned char bytes[3];
-    fread(bytes, 3, 1, file);
-
-    if (bytes[0] == 0xff && bytes[1] == 0xd8 && bytes[2] == 0xff)
-        return true;
-    else
-        return false;
-
-    return 0;
 }
 
 std::string ImageSet::getCurrentFile()
