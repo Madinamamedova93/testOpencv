e62e920885b966fd5e2805c03f471e9234b9ce2f
Madinamamedova93 <madinamamedova93@gmail.com>
2022-10-20 23:24:18 +0300

    Update pclProcessor.cpp

--- testOpenCV/pclProcessor.cpp
+++ testOpenCV/pclProcessor.cpp
@@ -15,7 +15,6 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr img_to_cloud(std::shared_ptr<cv::Mat> image)
 {
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>());
     if (image->type() == CV_8UC1) {
-        std::cout<<"first if"<<std::endl;
       for (int r = 0; r < image->rows; ++r) {
         for (int c = 0; c < image->cols; ++c) {
           if (image->at<unsigned char>(r, c) > 5) {
@@ -23,7 +22,6 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr img_to_cloud(std::shared_ptr<cv::Mat> image)
           }
         }
       }
-      std::cout<<"OK"<<std::endl;
     } else {
       std::cout << "std::vector<pcl::PointXYZRGB> convertMatToPointsCloud(const "
                    "cv::Mat& img) need CV_8UC1 mat as input. Current type is:"
