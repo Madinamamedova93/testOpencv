efaea9c05fa767ba8fde8d63d004480035a15548
Madinamamedova93 <madinamamedova93@gmail.com>
2022-10-20 23:01:01 +0300

    Update pclProcessor.h

--- testOpenCV/pclProcessor.h
+++ testOpenCV/pclProcessor.h
@@ -7,6 +7,5 @@
 #include <pcl/visualization/cloud_viewer.h>
 
 pcl::PointCloud<pcl::PointXYZ>::Ptr img_to_cloud(std::shared_ptr<cv::Mat> image);
-void SORFilter(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud);
 
 #endif // VISUALIZATION_H
